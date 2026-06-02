9287d381365793c7e347fdce1eb9f205521d27ba
tan21122008-eng <tan21122008@gmail.com>
2026-06-03 03:32:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ def home():
     return "Bot đang chạy!"
 
 def run():
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
